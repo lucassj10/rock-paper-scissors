e246987ee1d31e67ce94466400253eb5097148a2
lucassj10 <lucas.santanna.justino@gmail.com>
2025-02-18 23:21:51 -0300

    Create Function to get computer and Human imputs
    Also added variables for the score counter

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log(`Hello World`)
+function getComputerChoice(){
+    let radnomNumber = Math.floor( Math.random() * 4 );
+    let computerChoice
+    if ( radnomNumber === 1 ){
+        computerChoice = "rock";
+    } else if( radnomNumber === 2){
+        computerChoice = "paper";
+    } else{
+        computerChoice = "scissor";
+    }
+    return computerChoice
+}
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanChoice = prompt( "Rock, Paper or Scissors" ).toLowerCase();
+    return humanChoice;
+}
+console.log(getHumanChoice());
+
+let humanScore = 0
+let computerScore = 0
