3f795225de1305ace472d5450a25ce3002f2ac5f
lucassj10 <lucas.santanna.justino@gmail.com>
2025-02-20 23:46:09 -0300

    finish conditional statements of playRound function

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ function getComputerChoice(){
     } else if( radnomNumber === 1){
         computerChoice = "paper";
     } else{
-        computerChoice = "scissors";
+        computerChoice = "scissor";
     }
     return computerChoice
 }
 
 
 function getHumanChoice(){
-    let humanChoice = prompt( "Rock, Paper or Scissors" ).toLowerCase();
+    let humanChoice = prompt( "Rock, Paper or Scissor" ).toLowerCase();
     return humanChoice;
 }
 
@@ -23,10 +23,18 @@ let computerScore = 0;
 function playRound(humanImput, computerImput){
     if ( humanImput === "rock" && computerImput === "paper"){
         return console.log("You lose! Paper beats rock!");
-    } else if ( humanImput === "rock" && computerImput === "scissors" ) {
-        return console.log("You win! Rock beats Scissors!");
+    } else if ( humanImput === "rock" && computerImput === "scissor" ) {
+        return console.log("You win! Rock beats scissors!");
     } else if ( humanImput === "paper" && computerImput === "rock" ) {
         return console.log("You win! Paper beats rock!");
+      } else if ( humanImput === "paper" && computerImput === "scissor"){
+        return console.log("You lose! Scissor beats paper!")
+      }else if ( humanImput === "scissor" && computerImput === "rock"){
+        return console.log("You lose! Rock beats scissor!")
+      } else if ( humanImput === "scissor" && computerImput === "paper"){
+        return console.log("You win! Scissor beats paper!")
+      }else{
+        return console.log("It's a draw!")
       }
 }
 
