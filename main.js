d67b56f07c2f6f99c60f3aaaa4efbbb238b2c154
lucassj10 <lucas.santanna.justino@gmail.com>
2025-02-21 00:40:36 -0300

    create playGame function

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function getComputerChoice(){
     } else{
         computerChoice = "scissor";
     }
-    return computerChoice
+    console.log(computerChoice)
+    return computerChoice;
 }
 
 
@@ -22,23 +23,43 @@ let computerScore = 0;
 
 function playRound(humanImput, computerImput){
     if ( humanImput === "rock" && computerImput === "paper"){
+        computerScore = ++computerScore;
         return console.log("You lose! Paper beats rock!");
     } else if ( humanImput === "rock" && computerImput === "scissor" ) {
+        humanScore = ++humanScore;
         return console.log("You win! Rock beats scissors!");
     } else if ( humanImput === "paper" && computerImput === "rock" ) {
+        humanScore = ++humanScore;
         return console.log("You win! Paper beats rock!");
       } else if ( humanImput === "paper" && computerImput === "scissor"){
+        computerScore = ++computerScore;
         return console.log("You lose! Scissor beats paper!")
       }else if ( humanImput === "scissor" && computerImput === "rock"){
+        computerScore = ++computerScore;
         return console.log("You lose! Rock beats scissor!")
       } else if ( humanImput === "scissor" && computerImput === "paper"){
+        humanScore = ++humanScore;
         return console.log("You win! Scissor beats paper!")
       }else{
         return console.log("It's a draw!")
       }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
 
+function playGame (){
 playRound(humanSelection, computerSelection);
+}
+
+for ( let i = 1; i < 6; i++ ){
+playGame();
+}
+
+if ( computerScore > humanScore){
+    console.log("You lost the game!");
+ } else if ( computerScore > humanScore ) {
+    console.log("You Won the Game!");
+ } else{
+    console.log("It's a draw!")
+ }
